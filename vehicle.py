eba3689ec4ac9000b37d82a5e7796cb0513c313c
Bimbim4201 <149236597+Bimbim4201@users.noreply.github.com>
2023-11-20 14:02:39 +0700

    menambahkan if else dan def
    menambahkan def stop_engine kedalam class Car dan Motorcycle,
    memasukan if else kedalam class Car dan Motorcycle didalam def start_engine & stop_engine 
    
    lalu menambahkan code stop_engine di masing masing objek

--- vehicle.py
+++ vehicle.py
@@ -28,22 +28,52 @@ class Vehicle:
 
 class Car(Vehicle):
     def start_engine(self):
-        print(f"Menyalakan Mesin Mobil: {self.make} {self.model} ({self.year})...")
-        time.sleep(2)
-        print("Vroom! Mesin Mobil Menyala!\n")
+        if self.is_engine_started:
+            print("Mesin mobil sudah menyala!\n")
+        else:
+            print(f"Menyalakan mesin mobil: {self.make} {self.model} ({self.year})...")
+            time.sleep(2)
+            self.is_engine_started = True
+            print("Vroom! Mesin mobil menyala!\n")
+
+    def stop_engine(self):
+        if not self.is_engine_started:
+            print("Mesin mobil sudah mati!\n")
+        else:
+            print(f"Mematikan mesin mobil: {self.make} {self.model} ({self.year})...")
+            time.sleep(2)
+            self.is_engine_started = False
+            print("Mesin mobil dimatikan!\n")
 
 
 class Motorcycle(Vehicle):
     def start_engine(self):
-        print(f"Menyalakan Mesin Motor: {self.make} {self.model} ({self.year})...")
-        time.sleep(2)
-        print("Vroom! Mesin Motor Menyala!\n")
+        if self.is_engine_started:
+            print("Mesin motor sudah menyala!\n")
+        else:
+            print(f"Menyalakan mesin motor: {self.make} {self.model} ({self.year})...")
+            time.sleep(2)
+            self.is_engine_started = True
+            print("Vroom! Mesin motor menyala!\n")
+
+    def stop_engine(self):
+        if not self.is_engine_started:
+            print("Mesin motor sudah mati!\n")
+        else:
+            print(f"Mematikan mesin motor: {self.make} {self.model} ({self.year})...")
+            time.sleep(2)
+            self.is_engine_started = False
+            print("Mesin motor dimatikan!\n")
 
 
 # Membuat objek Car
 car = Car("Toyota", "Camry", 2022)
 car.start_engine()
+car.stop_engine()
+car.start_engine()
 
 # Membuat objek Motorcycle
 motorcycle = Motorcycle("Honda", "CBR500R", 2021)
+motorcycle.start_engine()
+motorcycle.stop_engine()
 motorcycle.start_engine()
